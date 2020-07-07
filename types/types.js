8bf0ec51f8e3642266884cf8b6d0969d5ccb34f7
Thaiane Braga <thaianefbraga@gmail.com>
2020-07-07 11:03:44 -0300

    Adds example of simple typing

--- types/types.js
+++ types/types.js
@@ -18,3 +18,22 @@ console.log(possuiHabilidades);
 var minhaIdade;
 minhaIdade = 27;
 console.log(minhaIdade);
+//array
+var hobbies = ['violão', 4];
+console.log(hobbies);
+//Tuplas: define quantidade e tipo fixo (estrutura typescript)
+var endereco = ['Av. Valparaiso', 1010];
+console.log(endereco);
+//enum (estrutura do typescript)
+var Cor;
+(function (Cor) {
+    Cor[Cor["Azul"] = 0] = "Azul";
+    Cor[Cor["Amarelo"] = 1] = "Amarelo";
+    Cor[Cor["Verde"] = 2] = "Verde";
+})(Cor || (Cor = {}));
+console.log(Cor.Amarelo);
+//any: permite qualquer valor, tipagem fraca e dinâmica. Usado bastante na graduação de JS para TS
+var carro = 'fusca';
+console.log(carro);
+carro = { modelo: 'fusca', ano: 92 };
+console.log(carro);
--- types/types.ts
+++ types/types.ts
@@ -22,3 +22,26 @@ console.log(possuiHabilidades)
 let minhaIdade: number
 minhaIdade = 27
 console.log(minhaIdade)
+
+//array
+let hobbies: any[] = ['violão', 4]
+console.log(hobbies)
+
+//Tuplas: define quantidade e tipo fixo (estrutura typescript)
+let endereco: [string, number] = ['Av. Valparaiso', 1010]
+console.log(endereco)
+
+//enum (estrutura do typescript)
+enum Cor {
+    Azul,
+    Amarelo,
+    Verde
+}
+
+console.log(Cor.Amarelo)
+
+//any: permite qualquer valor, tipagem fraca e dinâmica. Usado bastante na graduação de JS para TS
+let carro: any = 'fusca'
+console.log(carro)
+carro = { modelo: 'fusca', ano: 92 }
+console.log(carro)
