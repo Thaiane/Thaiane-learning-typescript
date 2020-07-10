26e8e9ea8e98540725d59e40af55986cec744243
Thaiane Braga <thaianefbraga@gmail.com>
2020-07-10 14:39:48 -0300

    Add more examples

--- ecmascript/emacScriptEx.js
+++ ecmascript/emacScriptEx.js
@@ -1,4 +1,76 @@
 "use strict";
+// Arrow Function
+function somar(n1, n2) {
+    return n1 + n2;
+}
+console.log(somar(2, 2));
+const subtrair = (n1, n2) => n1 - n2;
+console.log(subtrair(2, 3));
+const saudacao = () => console.log("Olá!");
+saudacao();
+const falarCom = (pessoa) => console.log('Ola ' + pessoa);
+falarCom('Thai');
+this;
+function normalComThis() {
+    console.log(this);
+}
+normalComThis();
+const normalComThisEspecial = normalComThis
+    .bind({ nome: 'Ana' });
+normalComThisEspecial();
+// this???
+console.log(this);
+const arrowComThis = () => console.log(this);
+arrowComThis();
+const arrowComThisEspecial = arrowComThis
+    .bind({ nome: 'Ana' });
+arrowComThisEspecial();
+// Rest & Spread
+const numbers = [1, 10, 99, -5, 200, 1034];
+console.log(Math.max(...numbers));
+const turmaA = ['João', 'Maria', 'Fernanda'];
+const turmaB = ['Fernando', ...turmaA, 'Miguel', 'Lorena'];
+console.log(turmaB);
+function retornarArray(...args) {
+    return args;
+}
+const numeros = retornarArray(1, 2, 4, 5, 6, 345, 623);
+console.log(numeros);
+console.log(retornarArray(...numbers));
+// Rest & Spread (Tupla)
+const tupla = [1, 'abc', false];
+function tuplaParam1(a, b, c) {
+    console.log(`1) ${a} ${b} ${c}`);
+}
+tuplaParam1(...tupla);
+function tuplaParam2(...params) {
+    // console.log(Array.isArray(params))
+    console.log(`2) ${params[0]} ${params[1]} ${params[2]}`);
+}
+tuplaParam2(...tupla);
+// Destructuring (array)
+const caracteristicas = ['Motor Zetec 1.8', 2020];
+// const motor = caracteristicas[0]
+// const ano = caracteristicas[1]
+const [motor, ano] = caracteristicas;
+console.log(motor);
+console.log(ano);
+// Destructuring (objeto)
+const item = {
+    nome: 'SSD 480GB',
+    preco: 200,
+    caracteristicas: {
+        w: 'Importado'
+    }
+};
+const nomeItem = item.nome;
+const precoItem = item.preco;
+console.log(nomeItem);
+console.log(precoItem);
+const { nome: n, preco: p, caracteristicas: { w } } = item;
+console.log(n);
+console.log(p);
+console.log(w);
 // Exercicio 1
 const dobro = (valor) => valor * 2;
 console.log(dobro(10));
@@ -26,3 +98,28 @@ fetch('https://swapi.dev/api/people/1')
     .then(res => console.log(res))
     .catch(rej => console.log(`Errrooouuuu ${rej}`));
 //TODO: O Exercicio 7 está com erro de CORS. Rever novamente mais tarde.
+// Callback
+function esperar3s(callback) {
+    setTimeout(() => {
+        callback('cb 3s depois...');
+    }, 3000);
+}
+esperar3s(function (resultado) {
+    console.log(resultado);
+});
+function esperar3sPromise() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('p 3s depois promise...');
+        }, 3000);
+    });
+}
+esperar3sPromise()
+    .then(dado => console.log(dado));
+fetch('https://swapi.dev/api/people/1')
+    .then(res => res.json())
+    .then(personagem => personagem.films)
+    .then(films => fetch(films[0]))
+    .then(resFilm => resFilm.json())
+    .then(filme => console.log(filme.title))
+    .catch(err => console.log('Catch!!!!' + err));
--- ecmascript/emacScriptEx.ts
+++ ecmascript/emacScriptEx.ts
@@ -1,3 +1,100 @@
+// Arrow Function
+function somar(n1: number, n2: number): number {
+    return n1 + n2
+}
+console.log(somar(2, 2))
+
+const subtrair = (n1: number, n2: number): number => n1 - n2
+console.log(subtrair(2, 3))
+
+const saudacao = () => console.log("Olá!")
+saudacao()
+
+const falarCom = (pessoa: string) => console.log('Ola ' + pessoa)
+falarCom('Thai')
+
+this
+
+function normalComThis() {
+    console.log(this)
+}
+normalComThis()
+
+const normalComThisEspecial = normalComThis
+    .bind({ nome: 'Ana' })
+normalComThisEspecial()
+
+// this???
+console.log(this)
+const arrowComThis = () => console.log(this)
+arrowComThis()
+
+const arrowComThisEspecial = arrowComThis
+    .bind({ nome: 'Ana' })
+arrowComThisEspecial()
+
+
+// Rest & Spread
+const numbers = [1, 10, 99, -5, 200, 1034]
+console.log(Math.max(...numbers))
+
+const turmaA: string[] = ['João', 'Maria', 'Fernanda']
+const turmaB: string[] =
+    ['Fernando', ...turmaA, 'Miguel', 'Lorena']
+console.log(turmaB)
+
+function retornarArray(...args: number[]): number[] {
+    return args
+}
+
+const numeros = retornarArray(1, 2, 4, 5, 6, 345, 623)
+console.log(numeros)
+console.log(retornarArray(...numbers))
+
+// Rest & Spread (Tupla)
+const tupla: [number, string, boolean] = [1, 'abc', false]
+
+function tuplaParam1(a: number, b: string, c: boolean): void {
+    console.log(`1) ${a} ${b} ${c}`)
+}
+
+tuplaParam1(...tupla)
+
+function tuplaParam2(...params: [number, string, boolean]) {
+    // console.log(Array.isArray(params))
+    console.log(`2) ${params[0]} ${params[1]} ${params[2]}`)
+}
+
+tuplaParam2(...tupla)
+
+// Destructuring (array)
+const caracteristicas = ['Motor Zetec 1.8', 2020]
+// const motor = caracteristicas[0]
+// const ano = caracteristicas[1]
+
+const [motor, ano] = caracteristicas
+console.log(motor)
+console.log(ano)
+
+// Destructuring (objeto)
+const item = {
+    nome: 'SSD 480GB',
+    preco: 200,
+    caracteristicas: {
+        w: 'Importado'
+    }
+}
+
+const nomeItem = item.nome
+const precoItem = item.preco
+console.log(nomeItem)
+console.log(precoItem)
+
+const { nome: n, preco: p, caracteristicas: { w } } = item
+console.log(n)
+console.log(p)
+console.log(w)
+
 // Exercicio 1
 const dobro = (valor: number): number => valor * 2
 console.log(dobro(10))
@@ -34,3 +131,35 @@ fetch('https://swapi.dev/api/people/1')
     .catch(rej => console.log(`Errrooouuuu ${rej}`))
 
 //TODO: O Exercicio 7 está com erro de CORS. Rever novamente mais tarde.
+
+// Callback
+
+function esperar3s(callback: (dado: string) => void) {
+    setTimeout(() => {
+        callback('cb 3s depois...')
+    }, 3000)
+}
+
+esperar3s(function(resultado: string) {
+    console.log(resultado)
+})
+
+function esperar3sPromise() {
+    return new Promise((resolve: any) => {
+        setTimeout(() => {
+            resolve('p 3s depois promise...')
+        }, 3000)
+    })
+}
+
+esperar3sPromise()
+    .then(dado => console.log(dado))
+
+fetch('https://swapi.dev/api/people/1')
+    .then(res => res.json())
+    .then(personagem => personagem.films)
+    .then(films => fetch(films[0]))
+    .then(resFilm => resFilm.json())
+    .then(filme => console.log(filme.title))
+    .catch(err => console.log('Catch!!!!' + err))
+
