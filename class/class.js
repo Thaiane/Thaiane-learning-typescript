d39459a4b80d3e50aa2b597f13c6512dfd65c6ef
Thaiane Braga <thaianefbraga@gmail.com>
2020-07-11 20:19:08 -0300

    Add class using typescript

---
+++ class/class.js
@@ -0,0 +1,15 @@
+"use strict";
+class Produto {
+    constructor(name, preco, desconto = 0) {
+        this.name = name;
+        this.preco = preco;
+        this.desconto = desconto;
+        console.log(`Construiu um produto com nome ${name}, preço de ${preco} e desconto de ${desconto}`);
+    }
+}
+let jogo = new Produto("Life is Strange", 145.34, 0.10);
+console.log(`Produto jogo`, jogo);
+jogo.desconto = 0.20;
+console.log(`Produto jogo agora`, jogo);
+let consoleGame = new Produto("PlayStation", 300.00);
+console.log(`Produto consoleGame`, consoleGame);
---
+++ class/class.ts
@@ -0,0 +1,17 @@
+class Produto {
+    constructor(
+        public name: string,
+        public preco: number,
+        public desconto: number = 0) {
+
+        console.log(`Construiu um produto com nome ${name}, preço de ${preco} e desconto de ${desconto}`)
+    }
+}
+
+let jogo = new Produto("Life is Strange", 145.34, 0.10)
+console.log(`Produto jogo`, jogo)
+jogo.desconto = 0.20
+console.log(`Produto jogo agora`, jogo)
+
+let consoleGame = new Produto("PlayStation", 300.00)
+console.log(`Produto consoleGame`, consoleGame)
