c4863e5cdd50542bb2eb4af59ab206c2c2a5fe6a
Thaiane Braga <thaianefbraga@gmail.com>
2020-07-13 12:35:43 -0300

    Add more examples about class using typescript

--- class/desafio.js
+++ class/desafio.js
@@ -19,44 +19,45 @@ moto.acelerar(30);
 console.log(moto.velocidade);
 moto.acelerar(10);
 console.log(moto.velocidade);
-//
-//
-// // Exercício 2 - Herança
-// var objeto2D = {
-//     base: 0,
-//     altura: 0
-// }
-//
-// var retangulo = Object.create(objeto2D)
-// retangulo.base = 5
-// retangulo.altura = 7
-// retangulo.area = function() {
-//     return this.base * this.altura
-// }
-// console.log(retangulo.area())
-//
-// // Exercício 3 - Getters & Setters
-// var estagiario = {
-//     _primeiroNome: ''
-// }
-//
-// Object.defineProperty(estagiario, 'primeiroNome', {
-//     get: function () {
-//         return this._primeiroNome
-//     },
-//     set: function (valor) {
-//         if (valor.length >= 3) {
-//             this._primeiroNome = valor
-//         } else {
-//             this._primeiroNome = ''
-//         }
-//     },
-//     enumerable: true,
-//     configurable: true
-// })
-//
-// console.log(estagiario.primeiroNome)
-// estagiario.primeiroNome = 'Le'
-// console.log(estagiario.primeiroNome)
-// estagiario.primeiroNome = 'Leonardo'
-// console.log(estagiario.primeiroNome)
+// Exercício 2 - Herança
+class Objeto2D {
+    constructor(base = 0, altura = 0) {
+        this.base = base;
+        this.altura = altura;
+    }
+}
+class Retangulo extends Objeto2D {
+    area() {
+        return this.base * this.altura;
+    }
+}
+const retangulo = new Retangulo(5, 5);
+retangulo.area = function () {
+    return this.base * this.altura;
+};
+console.log(retangulo.area());
+// Exercício 3 - Getters & Setters
+class Estagiario {
+    constructor(_primeiroNome = '', enumerable = true, configurable = true) {
+        this._primeiroNome = _primeiroNome;
+        this.enumerable = enumerable;
+        this.configurable = configurable;
+    }
+    set primeiroNome(valor) {
+        if (valor.length >= 3) {
+            this._primeiroNome = valor;
+        }
+        else {
+            this._primeiroNome = '';
+        }
+    }
+    get primeiroNome() {
+        return this._primeiroNome;
+    }
+}
+const estagiario = new Estagiario();
+console.log(estagiario.primeiroNome);
+estagiario.primeiroNome = 'Thai';
+console.log(estagiario.primeiroNome);
+estagiario.primeiroNome = 'Thaiane';
+console.log(estagiario.primeiroNome);
--- class/desafio.ts
+++ class/desafio.ts
@@ -26,44 +26,52 @@ console.log(moto.velocidade)
 moto.acelerar(10)
 console.log(moto.velocidade)
 
-//
-//
-// // Exercício 2 - Herança
-// var objeto2D = {
-//     base: 0,
-//     altura: 0
-// }
-//
-// var retangulo = Object.create(objeto2D)
-// retangulo.base = 5
-// retangulo.altura = 7
-// retangulo.area = function() {
-//     return this.base * this.altura
-// }
-// console.log(retangulo.area())
-//
-// // Exercício 3 - Getters & Setters
-// var estagiario = {
-//     _primeiroNome: ''
-// }
-//
-// Object.defineProperty(estagiario, 'primeiroNome', {
-//     get: function () {
-//         return this._primeiroNome
-//     },
-//     set: function (valor) {
-//         if (valor.length >= 3) {
-//             this._primeiroNome = valor
-//         } else {
-//             this._primeiroNome = ''
-//         }
-//     },
-//     enumerable: true,
-//     configurable: true
-// })
-//
-// console.log(estagiario.primeiroNome)
-// estagiario.primeiroNome = 'Le'
-// console.log(estagiario.primeiroNome)
-// estagiario.primeiroNome = 'Leonardo'
-// console.log(estagiario.primeiroNome)
+
+
+// Exercício 2 - Herança
+abstract class Objeto2D {
+    constructor(public base: number = 0, public altura: number = 0) {
+    }
+    abstract area(): number;
+}
+
+class Retangulo extends Objeto2D {
+    area(): number {
+        return this.base * this.altura;
+    }
+}
+
+const retangulo = new Retangulo(5,5);
+retangulo.area = function() {
+    return this.base * this.altura
+}
+console.log(retangulo.area())
+
+// Exercício 3 - Getters & Setters
+class Estagiario {
+    constructor(
+        private _primeiroNome: string = '',
+        public enumerable: boolean = true,
+        public configurable: boolean = true
+    ) { }
+
+    set primeiroNome(valor: string) {
+        if (valor.length >= 3) {
+            this._primeiroNome = valor
+        } else {
+            this._primeiroNome = ''
+        }
+    }
+
+    get primeiroNome(): string {
+        return this._primeiroNome;
+    }
+}
+
+const estagiario = new Estagiario();
+
+console.log(estagiario.primeiroNome)
+estagiario.primeiroNome = 'Thai'
+console.log(estagiario.primeiroNome)
+estagiario.primeiroNome = 'Thaiane'
+console.log(estagiario.primeiroNome)
