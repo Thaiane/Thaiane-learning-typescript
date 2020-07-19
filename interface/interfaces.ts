7acc3848ef0a2f3349e0fa9e40a2789b00a0f1b1
Thaiane Braga <thaianefbraga@gmail.com>
2020-07-19 18:45:49 -0300

    Add abstract class using interface

--- interface/interfaces.ts
+++ interface/interfaces.ts
@@ -21,3 +21,37 @@ const pessoa: Humano = {
 }
 
 saudar.saudar(pessoa)
+
+// Herança de interfaces
+interface A {
+  a(): void
+}
+
+interface B {
+  b(): void
+}
+
+interface ABC extends A, B {
+  c(): void
+}
+
+class RealA implements A {
+  a(): void {}
+}
+
+class RealAB implements A, B {
+  a(): void {}
+  b(): void {}
+}
+
+class RealABC implements ABC {
+  a(): void {}
+  b(): void {}
+  c(): void {}
+}
+
+abstract class AbsABD implements A, B {
+  a(): void {}
+  b(): void {}
+  abstract d(): void
+}
