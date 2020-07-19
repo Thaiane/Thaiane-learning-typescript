f2ec1f427d6d79c55295cfbe4148d9e5b9ab0e1a
Thaiane Braga <thaianefbraga@gmail.com>
2020-07-19 19:26:32 -0300

    Add more examples about generics

--- generics/generics.js
+++ generics/generics.js
@@ -11,9 +11,36 @@ function echoComGenerics(objeto) {
     return objeto;
 }
 console.log(echoComGenerics('Thai').length); //Podemos deiaxr explicito o tipo T na hora do uso
-console.log(echoComGenerics(28).length); // erro é validado pelo typescript antes da execução
+console.log(echoComGenerics(28)); // erro é validado pelo typescript antes da execução
 console.log(echoComGenerics({ nome: 'Thai', idade: 28 })); // TS infere o tipo de T conforme o tipo do objeto passado para ele
 const avaliaçoes = [4, 4, 5];
-avaliaçoes.push('3'); // erro porque nao é number
+// avaliaçoes.push('3') // erro porque nao é number
 avaliaçoes.push(4);
 console.log(avaliaçoes);
+// Array
+function imprimir(args) {
+    args.forEach(elemento => console.log(elemento));
+}
+imprimir([1, 2, 3]);
+imprimir([1, 2, 3]);
+imprimir(['Ana', 'Bia', 'Carlos']);
+imprimir([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 }
+]);
+imprimir([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 }
+]);
+const chamarEcho = echoComGenerics;
+console.log(chamarEcho('Alguma coisa'));
+// Class com Generics
+class OperacaoBinaria {
+    constructor(operando1, operando2) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+    }
+}
+// console.log(new OperacaoBinaria('Bom ', 'dia').executar())
--- generics/generics.ts
+++ generics/generics.ts
@@ -14,12 +14,50 @@ function echoComGenerics<T>(objeto: T): T {
 }
 
 console.log(echoComGenerics<string>('Thai').length); //Podemos deiaxr explicito o tipo T na hora do uso
-console.log(echoComGenerics(28).length) // erro é validado pelo typescript antes da execução
+console.log(echoComGenerics(28)) // erro é validado pelo typescript antes da execução
 console.log(echoComGenerics({ nome: 'Thai', idade: 28})) // TS infere o tipo de T conforme o tipo do objeto passado para ele
 
 const avaliaçoes: Array<number> = [4, 4, 5]
 
-avaliaçoes.push('3') // erro porque nao é number
+// avaliaçoes.push('3') // erro porque nao é number
 avaliaçoes.push(4)
 
 console.log(avaliaçoes)
+
+// Array
+function imprimir<T>(args: T[]) {
+    args.forEach(elemento => console.log(elemento))
+}
+
+imprimir([1, 2, 3])
+imprimir<number>([1, 2, 3])
+imprimir<string>(['Ana', 'Bia', 'Carlos'])
+imprimir<{ nome: string, idade: number }>([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 }
+])
+
+// Tipo objeto
+type Aluno = { nome: string, idade: number }
+imprimir<Aluno>([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 }
+])
+
+// Tipo Genérico
+type Echo = <T>(data: T) => T
+const chamarEcho: Echo = echoComGenerics
+console.log(chamarEcho<string>('Alguma coisa'))
+
+// Class com Generics
+abstract class OperacaoBinaria<T, R> {
+    constructor(public operando1: T,
+                public operando2: T) {}
+
+    abstract executar(): R
+}
+
+// console.log(new OperacaoBinaria('Bom ', 'dia').executar())
+
